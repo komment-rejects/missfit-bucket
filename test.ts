9b7fd85dd37c70129da2fca26c1ffb60f93f71d2
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-16 22:34:03 +0000

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,13 @@
+
+const add = (x: number, y: number): number => x + y;
+
+const sub = ((x: number, y: number): number => x - y;
+
+const mul = (x: number, y: number): number => x * y;
+
+const div = (x: number, y: number): number => y === 0 ? 0 : x / y;
+
+const log = (x: number, y: number = 10): number => {
+  // change of base formula
+  return Math.log(x) / Math.log(b)
+};
